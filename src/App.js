c2fdb55f4a250d4c1409f127de04f441b474b942
Peter <scatchie13@gmail.com>
2024-01-18 00:06:13 +0600

    Test

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css'
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <header className="app-header">
         <h1>Book library app</h1>
       </header>
